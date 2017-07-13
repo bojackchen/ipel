0ece4518731e3e6d716a8bb5c842836e6b99d4fd
Feng CHEN <fchensecurity@163.com>
2017-07-13 16:55:14 +0800

    Reorganize the structure

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 $(document).on("click", ".nav a", function(item) {
-    // Collapse navbar automatically after click on an item
-    $("#ipelNavbar").collapse("hide");
+  // Collapse navbar automatically after click on an item
+  $("#ipelNavbar").collapse("hide");
 })
 
 $(document).ready(function() {
-    // Add smooth scrolling to all internal links
-    $("a[href='#ipel']").on("click", function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $("html, body").animate({
-                scrollTop: $(hash).offset().top
-            }, 600, function(){
-                window.location.hash = hash;
-            });
-        }
-    });
+  // Add smooth scrolling to all internal links
+  $("a[href='#ipel']").on("click", function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate({
+        scrollTop: $(hash).offset().top
+      }, 600, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
 })
