8e95efbc256ed5446adfe94bcc1dcb48cde95cc7
Feng CHEN <fchensecurity@163.com>
2017-07-17 15:11:26 +0800

    Update and add last modified

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ $(document).on("click", ".nav a", function(item) {
 })
 
 $(document).ready(function() {
+  // Last modified field
+  var date = document.lastModified;
+  document.getElementById("lastupdate").innerHTML = "Last modified: " + date;
+
   // Add smooth scrolling to all internal links
   $("a[href='#ipel']").on("click", function(event) {
     if (this.hash !== "") {
