2ddb6f4937b34ac79ef6820d42bb835f9273923e
Feng CHEN <fchensecurity@163.com>
2017-09-03 22:43:30 +0800

    Fastforward

--- js/main.js
+++ js/main.js
@@ -3,21 +3,21 @@ $(document).on("click", ".nav a", function(item) {
   $("#ipelNavbar").collapse("hide");
 })
 
+$(document).on("click", "a[href='#ipel']", function(item) {
+  // Add smooth scrolling to all internal links
+  if (this.hash !== "") {
+    event.preventDefault();
+    var hash = this.hash;
+    $("html, body").animate({
+      scrollTop: $(hash).offset().top
+    }, 600, function(){
+      window.location.hash = hash;
+    });
+  }
+})
+
 $(document).ready(function() {
   // Last modified field
   if (document.getElementById("lastupdate") !== null)
     document.getElementById("lastupdate").innerHTML = "Last modified: " + document.lastModified;
-
-  // Add smooth scrolling to all internal links
-  $("a[href='#ipel']").on("click", function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $("html, body").animate({
-        scrollTop: $(hash).offset().top
-      }, 600, function(){
-        window.location.hash = hash;
-      });
-    }
-  });
 })
