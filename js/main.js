7c3a17a92b377eef283afc3451cbdf0b6d33579a
Feng CHEN <fchensecurity@163.com>
2017-09-03 16:45:37 +0800

    Googlemap breakdown

--- js/main.js
+++ js/main.js
@@ -21,19 +21,3 @@ $(document).ready(function() {
   if (document.getElementById("lastupdate") !== null)
     document.getElementById("lastupdate").innerHTML = "Last modified: " + document.lastModified;
 })
-
-// Add googleMap display
-function initMap() {
-  var map = new google.maps.Map(document.getElementById("googlemap"), {
-    center: {lat: 22.336464, lng: 114.265033},
-    zoom: 17,
-    scrollwheel: false,
-    draggable: true,
-  });
-
-  var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(22.336464, 114.265033),
-  });
-  marker.setMap(map);
-}
-google.maps.event.addDomListener(window, "load", initMap);
