f04dc3d1f564cc282f501f8cd844d941ef4ea437
Bojack F. CHEN <bojackchen@foxmail.com>
2018-06-27 10:14:35 +0800

    Update gmap display

--- js/gmap.js
+++ js/gmap.js
@@ -1,14 +1,14 @@
 // Add googleMap display
 function initMap() {
   var map = new google.maps.Map(document.getElementById("googlemap"), {
-    center: {lat: 22.336464, lng: 114.265033},
+    center: {lat: 22.334703, lng: 114.263723},
     zoom: 17,
     scrollwheel: false,
     draggable: true,
   });
 
   var marker = new google.maps.Marker({
-    position: new google.maps.LatLng(22.336464, 114.265033),
+    position: new google.maps.LatLng(22.334703, 114.263723),
   });
   marker.setMap(map);
 }
